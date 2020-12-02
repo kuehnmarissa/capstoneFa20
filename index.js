274e752e208895b679c22370a09f666bb292f740
Ashwin Chirumamilla <ashwinchirumamilla@gmail.com>
2020-12-02 16:24:05 -0600

    Implemented EJS and Express template with webpages

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
-const content = require('fs').readFileSync(__dirname + '/ace.html', 'utf8');
+//const content = require('fs').readFileSync(__dirname + '/ace.html', 'utf8');
 const log = console.log;
-let code = `function foo(items) {
-    var x = "All this is syntax highlighted";
-    return x;
-}`;
+let code = "";
 let compiledCode;
 const express = require('express');
-const port = 3000;
+const app = express();
+const PORT = 3000;
+const pages = require('./routes/pages');
 
-const http = require('http').createServer((req, res) => {
-  // serve the index.html file
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Length', Buffer.byteLength(content));
-  res.end(content);
+
+const server = app.listen(PORT, function(){
+  log(`Listening on http://localhost:${PORT}`);
+
 });
 
-const io = require('socket.io')(http);
+//app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.get('/', pages.getHomePage);
 
-//Initalizes an active, listening server on localhost:3000
-http.listen(port, () =>  {
-  log(`Listening to active server on port ${port}...`);
-});
+const io = require('socket.io')(server);
 
-
-//setup up the network socket commands in callback functions
-//rebroadcasts the text
 io.on('connection', (socket) => {
-    log('Successful  connection...');
+    log('Successful  connection....');
     socket.emit('message', code);
+
     socket.on('message', (evt) => {
       log("id:" + socket.id);
       code = evt;
-      log(code);
+      //log(code);
       socket.broadcast.emit('message', evt)
-      compiledCode = eval(code);
-      log(compiledCode);
+
     });
 });
 
 io.on('disconnect', (evt) => {
   log('User disconnected...');
 });
+
+
+/*const http = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(content));
+  res.end(content);
+});*/
+
+
+
+
+//Initalizes an active, listening server on localhost:3000
+/*http.listen(port, () =>  {
+  log(`Listening to active server on port ${port}...`);
+});*/
+
+
+//setup up the network socket commands in callback functions
+//rebroadcasts the text
